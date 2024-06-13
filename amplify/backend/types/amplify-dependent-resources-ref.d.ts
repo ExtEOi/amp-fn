08381af454ac688df3c6f495c78e210c57d4202f
EC2 Default User <ec2-user@ip-172-31-29-27.ap-northeast-1.compute.internal>
2024-06-13 03:12:15 +0000

    Creation of a prod amplify environment

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "amp061142cf62c0": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "amp0611layera650cd6f": {
+      "Arn": "string"
+    }
+  }
+}
